d6b74d94b0d56130fd38338da49a7b98e53a674a
Nimtara Munasinghe <nimtaramunasinghe@gmail.com>
2024-05-11 13:27:42 +0530

    backend features

--- src/repositories/user.js
+++ src/repositories/user.js
@@ -17,70 +17,69 @@ export const signUpUser = async (data) => {
   }
 };
 
-export const removeUser = (uid) => {
-  auth
-    .deleteUser(uid)
-    .then(() => {
-      console.log("Successfully deleted user");
-      return {
-        message: `Successfully deleted user ${uid}`,
-      };
-    })
-    .catch((error) => {
-      console.log("Error deleting user:", error);
-    });
+export const removeUser = async (uid) => {
+  try {
+    const newUser = await auth.deleteUser(uid);
+    return {
+      message: `Successfully deleted user ${uid}`,
+    };
+  } catch (error) {
+    console.log("Error deleting user:", error);
+  }
 };
 
-export const updateUser = (data) => {
-  auth
-    .updateUser(uid, {
+export const updateUser = async (uid, data) => {
+  try {
+    const userRecord = await auth.updateUser(uid, {
       email: data.email,
       emailVerified: false,
       phoneNumber: data.phoneNumber,
       displayName: data.displayName,
       disabled: false,
-    })
-    .then((userRecord) => {
-      // See the UserRecord reference doc for the contents of userRecord.
-      console.log("Successfully updated user", userRecord.toJSON());
-      return {
-        message: "Successfully updated user",
-        payload: userRecord.toJSON(),
-      };
-    })
-    .catch((error) => {
-      console.log("Error updating user:", error);
     });
+    return {
+      message: "Successfully updated user",
+      payload: userRecord.toJSON(),
+    };
+  } catch (error) {
+    console.log("Error updating user:", error);
+  }
 };
 
-export const getUserByEmail = (email) => {
-  auth
-    .getUserByEmail(email)
-    .then((userRecord) => {
-      // See the UserRecord reference doc for the contents of userRecord.
-      console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
-      return {
-        message: "Successfully fetched user data",
-        payload: userRecord.toJSON(),
-      };
-    })
-    .catch((error) => {
-      console.log("Error fetching user data:", error);
-    });
+export const getUserByEmail = async (email) => {
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    return {
+      message: "Successfully fetched user data",
+      payload: userRecord.toJSON(),
+    };
+  } catch (error) {
+    console.log("Error fetching user data:", error);
+  }
 };
 
-export const getUserById = (uid) => {
-  auth
-    .getUser(uid)
-    .then((userRecord) => {
-      // See the UserRecord reference doc for the contents of userRecord.
-      console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
-      return {
-        message: "Successfully fetched user data",
-        payload: userRecord.toJSON(),
-      };
-    })
-    .catch((error) => {
-      console.log("Error fetching user data:", error);
+export const getUserById = async (uid) => {
+  try {
+    const userRecord = await auth.getUser(uid);
+    return {
+      message: "Successfully fetched user data",
+      payload: userRecord.toJSON(),
+    };
+  } catch (error) {
+    console.log("Error fetching user data:", error);
+  }
+};
+
+// reset password
+export const resetPassword = async (uid, data) => {
+  try {
+    await auth.updatePassword(uid, {
+      password: data.password,
     });
+    return {
+      message: "Password is reset",
+    };
+  } catch (error) {
+    console.log("Error resetting password:", error);
+  }
 };
